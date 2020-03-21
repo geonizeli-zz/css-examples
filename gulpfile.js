eeda1e6065b4cd7971d3f9f76ade1dceeda38e27
João Victor <jvsouzax@gmail.com>
2020-03-20 23:57:27 -0300

    add: gulp

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+// Inicializa modulos
+const gulp = require('gulp');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+
+// Coloca o caminho dos arquivos aqui
+const files = {
+  scssPath: 'src/scss/*.scss',
+  cssDest: 'src/css',
+}
+// Sass task
+function scssTask() {
+
+  return gulp.src(files.scssPath)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(files.cssDest));
+
+}
+
+// Watch task
+function watchTask() {
+  gulp.watch('src/scss/*.scss', scssTask);
+}
+
+// Default task
+exports.default = gulp.series(
+  scssTask,
+  watchTask
+);
